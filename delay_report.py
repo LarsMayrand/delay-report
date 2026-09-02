e3a2183788a212663dcadd243fb9d55a6c67fb7d
Burkson <burkson@Burksons-MacBook-Neo.local>
2026-09-02 11:10:37 -0700

    progress

--- delay_report.py
+++ delay_report.py
@@ -1,21 +1,24 @@
+from matplotlib import colormaps
 from collections import Counter
 import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 from matplotlib.pyplot import figure
+from datetime import datetime 
+import numpy as np
 
-figure(figsize=(35, 5), dpi=80)
-
-# CG_DELAY_REASONS = ['Baggage', 'Resource'] cargo? 
-CG_DELAY_REASONS = ['Baggage', 'Resource', 'Equipment']
+CG_DELAY_REASONS = ['Baggage', 'Resource'] # cargo? potentially damage, 
+# CG_DELAY_REASONS = ['Baggage', 'Resource', 'Equipment']
 HUBS = ['LAX', 'IAD', 'ORD', 'EWR', 'IAH', 'DEN']
 WIDE_BODY = ['777', '77W', '788', '789', '763', '764']
 NARROW_BODY = ['319', '320', '321', '737', '738', '7M8', '739', '7M9','752', '753', ]
 EXPRESS = ['CR7', 'CRJ', 'E7W', 'ERJ', 'CR5']
-INTL = ['AKL', 'AMS', 'BCN', 'CHC', 'CDG', 'CUN', 'FCO', 'FRA', 'HKG', 'HND', 'ICN', 'KIX', 'LHR', 'MEL', 'MEX', 'MNL', 'MTY', 'MUC', 'NRT', 'PEK', 'PPT', 'PVG', 'PVR', 'SIN', 'SJD', 'SJO', 'SYD', 'TPE', 'YUL', 'YVR', 'YYC', 'YYZ', 'ZRH']
+INTL = ['AKL', 'AMS', 'BCN', 'BNE', 'CHC', 'CDG', 'CUN', 'FCO', 'FRA', 'HKG', 'HND', 'ICN', 
+    'KIX', 'LHR', 'MEL', 'MEX', 'MNL', 'MTY', 'MUC', 'NRT', 'PEK', 'PPT', 'PVG', 
+    'PVR', 'SIN', 'SJD', 'SJO', 'SYD', 'TPE', 'YUL', 'YVR', 'YYC', 'YYZ', 'ZRH']
+#---------------- I/O -----------------------#
 
-#---------------- Methods -----------------------#
 def get_data(folder):
     path = os.getcwd() + folder
     files = os.listdir(path)
@@ -26,21 +29,26 @@ def get_data(folder):
             df = pd.concat([df, data], ignore_index=True)
     return df
 
+def export(df, name):
+    df.to_excel(name + '.xlsx')
+
+#---------------- Filters -----------------------#
 def filter(df, filter):
     return df[df.apply(filter, axis=1)]
 
 def is_delay(f, criteria=None):
-    if (criteria is None):
-        return f['REASON'].split() != '--'
-    return f['REASON'].split()[0] in criteria
+    if (f.at['DELAY DEP'][0] == '-' or f.at['DELAY DEP'] == '00:00'):
+        return False
+    if (criteria is None): 
+        return True
+    return f['REASON'].split()[0] in criteria 
 
 def is_cg_delay(f):
     return is_delay(f, CG_DELAY_REASONS)
 
 def is_special(f, special):
     icon = f.at['SPECIALS']
-    if (type(icon) != str): return False
-    return special in icon.split() 
+    return False if type(icon) != str else special in icon.split() 
 
 def is_star(f):
     return is_special(f, 'S')
@@ -51,24 +59,47 @@ def is_priority(f):
 def is_quick_turn(f):
     return is_special(f, 'Q')
 
-def get_delays(df):
-    return df[df['REASON'] != '--']
+def is_intl(f):
+    return f['ARR'] in INTL
 
-# Calculates frequency of each delay type for all station delays 
-def get_station_reasons(df):
+def is_widebody(f):
+    return f['FLEET TYPE'] in WIDE_BODY
+
+def is_narrowbody(f):
+    return f['FLEET TYPE'] in NARROW_BODY
+
+def is_express(f):
+    return f['FLEET TYPE'] in EXPRESS
+
+def is_late_departure(f):
+    return datetime(f.at['SCHED DEP']) < datetime(f.at['ETD/ACT'])
+
+def is_late_arrival(f):
+    # return datetime(f.at['SCHED ARR']) < datetime(f.at['ETA/ACT'])
+    return f.at['DELAY AR'][0] != '-'
+
+def filter_by_zone(df, zone):
+    return df[df['DEP ZONE'] == zone]
+
+#----------------------- logic --------------#
+
+# Calculates frequency of the n most common delay types 
+def get_reasons(df, n = None):
     reasons = df['REASON']
     station_reasons = []
     for index, value in reasons.items():
         first_reason = value.split()[0]
         if (first_reason != '--'):
             station_reasons.append(first_reason)
-    return Counter(station_reasons).most_common(12)
+    return Counter(station_reasons).most_common(n)
 
-def filter_by_zone(df, zone):
-    return df[df['DEP ZONE'] == zone]
-
-def export(df, name):
-    df.to_excel(name + '.xlsx')
+def format_for_pie_chart(list, n):
+    other = 0
+    for i in range(10, len(list)):
+        other += list[i][1]
+    top_ten_reasons = station_delays_reasons[:10]
+    top_ten_reasons.append(('Other', other))
+    return top_ten_reasons
 
 def get_gates(df):
     gates = df['DEP GATE']
@@ -83,28 +114,74 @@ def get_aircrafts(df):
 
 '''delays per scheduled hour'''
 def get_delays_by_hour(df):
-
     return     
 
 #---------------- plots -----------------------#
 
-def bar_plot(df):
-    categories, values = zip(*df)
+def line_plot(d):
+    return
+
+def pie_chart(dict):
+    labels = list(dict.keys())
+    sizes = list(dict.values()) 
+    figure(figsize=(6, 6))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')  # Equal aspect ratio ensures the pie is circular
+    plt.title('Delays by fleet type')
+    plt.show()
+
+def pie_chart_fancy(dict):
+    figure(figsize=(6, 6))
+    plt.pie(list(dict.values()), 
+            labels = list(dict.keys()), 
+            explode = (0, 0, 0, 0.15, 0, 0, 0, 0, 0, 0),    
+            autopct = '%1.1f%%', 
+            colors = plt.cm.Pastel2.colors
+            # colors = plt.cm.tab20.colors
+    )
+    plt.axis('equal')
+    plt.show()
+
+def plot_station_pie(data):
+    delays = filter(data, is_delay)
+    station_delays_reasons = get_reasons(delays)
+    other = 0
+    for i in range(10, len(station_delays_reasons)):
+        # print(f'reason at i = {i}, {station_delays_reasons[i][0]}')
+        other += station_delays_reasons[i][1]
+    top_ten_reasons = station_delays_reasons[:10]
+    top_ten_reasons.append(('Other', other))
+    print(top_ten_reasons)
+    top_reasons = [('Aircraft-LT', 1121), ('Connections', 659), ('Crew', 617), ('Baggage / Resource', 415), 
+        ('Aircraft', 410), ('ATC', 239), ('Aircraft-New', 221), ('Servicing', 198), 
+        ('Customer', 186), ('Other', 307)]
+    pie_chart_fancy(dict(top_reasons))
+
+def bar_plot(d):
+    figure(figsize=(35, 5), dpi=80)
+    categories, values = zip(*d)
     plt.bar(categories, values)
     plt.show()   
 
 def plot_station_reasons(df):
-    bar_plot(get_station_reasons(df)) 
+    bar_plot(get_reasons(df)) 
 
 #---------------- info -----------------------------$
 
 def info(df):
     return {
         'total': len(df), 
-        # 'delays': len(get_delays(df)),
+        'delays': len(filter(df, is_delay)),
         'stars': len(filter(df, is_star)),
         'priority': len(filter(df, is_priority)),
         'quick turns': len(filter(df, is_quick_turn))
+    }
+
+def fleet_breakdown(df):
+    return {
+        'Widebody' : len(filter(df, is_widebody)), 
+        'Narrowbody' : len(filter(df, is_narrowbody)), 
+        'Express' : len(filter(df, is_express)), 
     }
 
 # Returns list of worst performing routes in dataframe
@@ -134,9 +211,6 @@ def desinations_info(df):
 #-------------------- main -------------------------------#
 
 if __name__ == '__main__':
-    data = get_data('/data/SFO-Aug/')
-    # cg_delays = filter(data, is_cg_delay)
-    # cg_info = info(cg_delays)
-    print(info(data))
-    print(desinations_info(data))
-    # plot_station_reasons(data)
+    data = get_data('/delay_report/data/SFO_AUG/')
+    plot_station_pie(data)
+    
---
+++ delay_report_notes.py
@@ -0,0 +1,22 @@
+delay_report_notes
+
+# print(station_delays_reasons)
+    # pie_chart(dict(station_delays_reasons))
+    # pie_chart_fancy(dict(format_for_pie_chart(station_delays_reasons, 9)))
+    # pie_chart_fancy(dict(station_delays_reasons))
+    
+    # fleet_numbers = fleet_breakdown(data)
+    # fleet_numbers = fleet_breakdown(filter(data, is_cg_delay))
+    # pie_chart_fancy(fleet_numbers)
+    # # print(type(fleet_numbers))
+    
+    # wb = filter(data, is_widebody)
+    # delays = filter(data, is_late_departure)
+    # print(len(delays))
+    # stars = filter(data, is_star)
+    # print(len(stars))
+    # cg_delays = filter(data, is_cg_delay)
+    # cg_info = info(cg_delays)
+    # print(info(data))
+    # print(desinations_info(data))
+    # plot_station_reasons(data)
